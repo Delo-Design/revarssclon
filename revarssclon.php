8f40a91a5154dd298779b416b56a63f2ad93ec6c
Dmitrii Cymbal <tsymbal.mitia@gmail.com>
2021-07-06 16:05:55 +0300

    заливка файлов

---
+++ revarssclon.php
@@ -0,0 +1,57 @@
+<?php defined('_JEXEC') or die;
+
+
+use Joomla\CMS\Application\CMSApplication;
+use Joomla\CMS\Plugin\CMSPlugin;
+
+
+/**
+ * Variablesubdomain plugin.
+ *
+ * @package   regionzoloto
+ * @since     1.0
+ */
+class plgRevarsRevarssclon extends CMSPlugin
+{
+
+	/**
+	 * Application object
+	 *
+	 * @var    CMSApplication
+	 * @since  1.0.0
+	 */
+	protected $app;
+
+
+	/**
+	 * Affects constructor behavior. If true, language files will be loaded automatically.
+	 *
+	 * @var    boolean
+	 * @since  1.0.0
+	 */
+	protected $autoloadLanguage = true;
+
+
+	public function onRevarsAddVariables()
+	{
+
+		$sclons  = $this->params->get('sclons');
+		$output  = [];
+		$columns = ['im', 'rod', 'dat', 'vin', 'tv', 'pr'];
+
+		foreach ($sclons as $sclon)
+		{
+			foreach ($columns as $column)
+			{
+				$output[] = (object) [
+					'variable' => '{VAR_' . strtoupper($sclon->name) . '_' . strtoupper($column) . '}',
+					'value'    => $sclon->$column ?? ''
+				];
+			}
+
+		}
+
+		return $output;
+	}
+
+}
